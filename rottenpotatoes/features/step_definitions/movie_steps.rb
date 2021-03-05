49015c03afe5ca16a975d18f7c325ae14eef72aa
Phi Thuan Au <ec2-user@ip-172-31-0-149.us-east-2.compute.internal>
2021-03-05 01:02:58 +0000

    Change steps for sort.

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -7,18 +7,7 @@ Given /the following movies exist/ do |movies_table|
 end
 
 Then /(.*) seed movies should exist/ do | n_seeds |
-  Movie.count.should be n_seeds.to_i
-end
-
-# Get the content from index 
-# Get the sorted order from db 
-# Compare each corresponding pair in the 2 lists
-Then /I should see movies sorted in increasing order of (.*)/ do |sort_by|
-  rows = page.all('#movies tbody tr')
-  movies = Movie.select('title').order(sort_by.parameterize.underscore)
-  rows.zip(movies).each do |row, movie|
-    expect(row.text).to have_text movie[:title]
-  end
+  expect(Movie.count).to be n_seeds.to_i
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -31,6 +20,12 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
     step %{I #{uncheck}check "ratings[#{rating}]"}
   end
 end
+
+# Using regex to search for one string before another 
+Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
+  expect(page.body).to match /#{e1}.*#{e2}/m
+end
+
 
 Then /I should (not )?see movies of the following ratings: (.*)/ do |invisible, rating_list|
   rating_list = rating_list.split(", ")
@@ -48,5 +43,5 @@ end
 
 Then /I should see all the movies/ do
   rows = page.all("#movies tbody tr").count
-  rows.should be Movie.count
+  expect(rows).to be Movie.count
 end
